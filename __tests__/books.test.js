62d4e3d570898158ecd94511b5227872119acd78
Rian <rgillard@nuccimedical.com>
2024-01-24 17:56:00 -0500

    Add files via upload

---
+++ __tests__/books.test.js
@@ -0,0 +1,135 @@
+process.env.NODE_ENV = "test";
+
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db");
+
+// isbn of sample book
+let book_isbn;
+
+beforeEach(async () => {
+  let result = await db.query(`
+    INSERT INTO
+      books (isbn, amazon_url, author, language, pages, publisher, title, year)
+      VALUES(
+        '123432122',
+        'https://amazon.com/taco',
+        'Elie',
+        'English',
+        100,
+        'Nothing publishers',
+        'my first book', 2008)
+      RETURNING isbn`);
+
+  book_isbn = result.rows[0].isbn;
+});
+
+describe("POST /books", function () {
+  test("Creates a new book", async function () {
+    const response = await request(app)
+      .post(`/books`)
+      .send({
+        isbn: '987654321',
+        amazon_url: "https://example.com",
+        author: "John Doe",
+        language: "Spanish",
+        pages: 250,
+        publisher: "Great Publishing",
+        title: "Awesome Book",
+        year: 2015,
+      });
+    expect(response.statusCode).toBe(201);
+    expect(response.body.book).toHaveProperty("isbn");
+  });
+
+  test("Prevents creating book without required title", async function () {
+    const response = await request(app)
+      .post(`/books`)
+      .send({ year: 2000 });
+    expect(response.statusCode).toBe(400);
+  });
+});
+
+describe("GET /books", function () {
+  test("Gets a list of 1 book", async function () {
+    const response = await request(app).get(`/books`);
+    const books = response.body.books;
+    expect(books).toHaveLength(1);
+    expect(books[0]).toHaveProperty("isbn");
+    expect(books[0]).toHaveProperty("amazon_url");
+  });
+});
+
+describe("GET /books/:isbn", function () {
+  test("Gets a single book", async function () {
+    const response = await request(app)
+      .get(`/books/${book_isbn}`);
+    expect(response.body.book).toHaveProperty("isbn");
+    expect(response.body.book.isbn).toBe(book_isbn);
+  });
+
+  test("Responds with 404 if can't find book in question", async function () {
+    const response = await request(app)
+      .get(`/books/999`);
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+describe("PUT /books/:id", function () {
+  test("Updates a single book", async function () {
+    const response = await request(app)
+      .put(`/books/${book_isbn}`)
+      .send({
+        amazon_url: "https://newexample.com",
+        author: "Jane Doe",
+        language: "French",
+        pages: 300,
+        publisher: "New Publishing",
+        title: "Updated Book",
+        year: 2020,
+      });
+    expect(response.body.book).toHaveProperty("isbn");
+    expect(response.body.book.title).toBe("Updated Book");
+  });
+
+  test("Prevents a bad book update", async function () {
+    const response = await request(app)
+      .put(`/books/${book_isbn}`)
+      .send({
+        isbn: "987654321",
+        badField: "DO NOT ADD ME!",
+        amazon_url: "https://newexample.com",
+        author: "Jane Doe",
+        language: "French",
+        pages: 300,
+        publisher: "New Publishing",
+        title: "Updated Book",
+        year: 2020,
+      });
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("Responds 404 if can't find book in question", async function () {
+    // delete book first
+    await request(app)
+      .delete(`/books/${book_isbn}`);
+    const response = await request(app).delete(`/books/${book_isbn}`);
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+describe("DELETE /books/:id", function () {
+  test("Deletes a single a book", async function () {
+    const response = await request(app)
+      .delete(`/books/${book_isbn}`);
+    expect(response.body).toEqual({ message: "Book deleted" });
+  });
+});
+
+afterEach(async function () {
+  await db.query("DELETE FROM BOOKS");
+});
+
+afterAll(async function () {
+  await db.end();
+});
